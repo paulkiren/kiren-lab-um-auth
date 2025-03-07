d70b2f48d5784a167a08400ca4547999f4a82f0b
Kiren Paul <kirenpaul@gmail.com>
2025-03-07 17:22:09 +0700

    Add User service implementation with CRUD operations and PostgreSQL configuration

---
+++ user-service/src/main/java/com/kirenlab/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+package com.kirenlab.userservice;
+
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
