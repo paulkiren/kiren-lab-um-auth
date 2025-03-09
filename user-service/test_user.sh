7e92ae193f58b2a5e55307af3a0a4c6c42941ccb
Kiren Paul <kirenpaul@gmail.com>
2025-03-09 11:09:14 +0700

    Add test script for user registration and authentication endpoints

--- user-service/test_user.sh
+++ user-service/test_user.sh
@@ -17,3 +17,19 @@ curl -X POST http://localhost:8081/users/register \
 
 curl -X GET http://localhost:8081/users
 curl -X GET http://localhost:8081/users/1
+
+
+# Path: user-service/test_user.sh
+curl -X POST http://localhost:8081/users/register \
+     -H "Content-Type: application/json" \
+     -d '{"username": "kiren", "password": "securepass", "email": "kiren@example.com", "role": "USER"}'
+
+
+curl -X POST http://localhost:8081/auth/login \
+     -H "Content-Type: application/json" \
+     -d '{"username": "kiren", "password": "securepass"}'
+
+    #  { "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." }
+
+curl -X GET http://localhost:8081/users \
+     -H "Authorization: Bearer YOUR_JWT_TOKEN"
